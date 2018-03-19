2a0d29d1757e6b49827ff2511adbf654eed61279
homer0 <leo.apiwan@olapic.com>
2018-03-19 15:24:43 -0300

    Add an abstract class for generators

---
+++ src/abstracts/cliGeneratorSubCommand.js
@@ -0,0 +1,128 @@
+/**
+ * A helper class for creating generator commands for the CLI. A generator command works like a
+ * {@link CLICommand} inside a regular {@link CLICommand}. It has a resource type that is in charge
+ * of generating and it supports options for customzation.
+ * @abstract
+ */
+class CLIGeneratorSubCommand {
+  /**
+   * Class constructor.
+   * @throws {TypeError} If instantiated directly.
+   * @abstract
+   */
+  constructor() {
+    if (new.target === CLIGeneratorSubCommand) {
+      throw new TypeError(
+        'CLIGeneratorSubCommand is an abstract class, it can\'t be instantiated directly'
+      );
+    }
+    /**
+     * An identifier for the resource the generator creates.
+     * @type {string}
+     */
+    this.resource = '';
+    /**
+     * A short description for what the generator does.
+     * @type {string}
+     */
+    this.description = '';
+    /**
+     * A list with the name of the options the generator supports. New options can be added using
+     * the `addOption` method.
+     * @type {Array}
+     */
+    this.options = [];
+    /**
+     * A dictionary of options settings by their option name. New options can be added using
+     * the `addOption` method.
+     * @type {Object}
+     */
+    this.optionsByName = {};
+  }
+  /**
+   * Add a new option for the command.
+   * @example
+   * // To capture an option
+   * this.addOption(
+   *   'type',
+   *   '-t, --type [type]',
+   *   'The type of thingy you want to use?',
+   * );
+   *
+   * // As a simple flag
+   * this.addOption(
+   *   'ready',
+   *   '-r, --ready',
+   *   'Is it read?',
+   *   false
+   * );
+   *
+   * @param {string} name              The option name.
+   * @param {string} instruction       The option instruction, for example: `-t, --type [type]`.
+   * @param {string} [description='']  The option description.
+   * @param {string} [defaultValue=''] The option default value, in case is not used on execution.
+   */
+  addOption(name, instruction, description = '', defaultValue = '') {
+    this.optionsByName[name] = {
+      name,
+      instruction,
+      description,
+      defaultValue,
+    };
+
+    this.options.push(name);
+  }
+  /**
+   * Generate a complete description of the generator and its options in order to be used on the
+   * help interface of the {@link CLICommand} that implements it.
+   * @return {string}
+   */
+  getHelpInformation() {
+    // Define the basic description line.
+    let description = ` - '${this.resource}': ${this.description}`;
+    // If the generator has options...
+    if (this.options.length) {
+      // ...add the options subtitle.
+      description += '\n   Options:\n';
+      /**
+       * Loop all the options and find the one with the longest instruction to calculate the
+       * necessary padding in order to show all instructions on a _"fixed width column"_ style.
+       */
+      let longest = '';
+      this.options.forEach((optionName) => {
+        const option = this.optionsByName[optionName];
+        if (option.instruction.length > longest.length) {
+          longest = option.instruction;
+        }
+      });
+      // Once the longest instruction is saved, loop all the options again...
+      this.options.forEach((optionName) => {
+        const option = this.optionsByName[optionName];
+        // Set the instruction as the initial value of the description.
+        let line = option.instruction;
+        // If the instruction is shorter than the longest, add the necessary padding.
+        if (line.length < longest.length) {
+          line += (new Array(longest.length - line.length)).fill(' ').join('');
+        }
+        // Add the option description after the instruction, and possibly the padding.
+        line += `  ${option.description}`;
+        // Add the description to the generator description.
+        description += `\n   ${line}`;
+      });
+    }
+
+    return description;
+  }
+  /**
+   * The method called by the {@link CLICommand} that implements the generator in order to create
+   * the resource.
+   * It receives a dicitionary with the parsed options the command received.
+   * @return {Promise<undefined,Error>}
+   * @abstract
+   */
+  generate() {
+    return Promise.reject(Error('This method must to be overwritten'));
+  }
+}
+
+module.exports = CLIGeneratorSubCommand;
---
+++ tests/abstracts/cliGeneratorSubCommand.test.js
@@ -0,0 +1,148 @@
+jest.unmock('/src/abstracts/cliGeneratorSubCommand');
+
+require('jasmine-expect');
+const CLIGeneratorSubCommand = require('/src/abstracts/cliGeneratorSubCommand');
+
+describe('abstracts:CLIGeneratorSubCommand', () => {
+  it('should throw an error if used without subclassing it', () => {
+    // Given/When/Then
+    expect(() => new CLIGeneratorSubCommand())
+    .toThrow(/CLIGeneratorSubCommand is an abstract class/i);
+  });
+
+  it('should be able to be instantiated when subclassed', () => {
+    // Given
+    class Sut extends CLIGeneratorSubCommand {}
+    let sut = null;
+    // When
+    sut = new Sut();
+    // Then
+    expect(sut).toBeInstanceOf(Sut);
+    expect(sut).toBeInstanceOf(CLIGeneratorSubCommand);
+  });
+
+  it('should be able to register new options', () => {
+    // Given
+    const optionOne = {
+      name: 'type',
+      instruction: '-t',
+    };
+    const optionTwo = {
+      name: 'env',
+      instruction: '-e',
+      description: 'Set the environment',
+    };
+    const optionThree = {
+      name: 'build',
+      instruction: '-b',
+      description: 'Set the build',
+      defaultValue: true,
+    };
+    const numberOfOptions = 3;
+    class Sut extends CLIGeneratorSubCommand {}
+    let sut = null;
+    // When
+    sut = new Sut();
+    sut.addOption(optionOne.name, optionOne.instruction);
+    sut.addOption(optionTwo.name, optionTwo.instruction, optionTwo.description);
+    sut.addOption(
+      optionThree.name,
+      optionThree.instruction,
+      optionThree.description,
+      optionThree.defaultValue
+    );
+    // Then
+    expect(sut.options.length).toBe(numberOfOptions);
+    expect(sut.options).toEqual([optionOne.name, optionTwo.name, optionThree.name]);
+    expect(sut.optionsByName[optionOne.name]).toEqual({
+      name: optionOne.name,
+      instruction: optionOne.instruction,
+      description: '',
+      defaultValue: '',
+    });
+    expect(sut.optionsByName[optionTwo.name]).toEqual({
+      name: optionTwo.name,
+      instruction: optionTwo.instruction,
+      description: optionTwo.description,
+      defaultValue: '',
+    });
+    expect(sut.optionsByName[optionThree.name]).toEqual({
+      name: optionThree.name,
+      instruction: optionThree.instruction,
+      description: optionThree.description,
+      defaultValue: optionThree.defaultValue,
+    });
+  });
+
+  it('should be able to generate a help for the CLI', () => {
+    // Given
+    const resource = 'my-resource';
+    const description = 'Something';
+    class Sut extends CLIGeneratorSubCommand {}
+    let sut = null;
+    let result = null;
+    const expectedDescription = ` - '${resource}': ${description}`;
+    // When
+    sut = new Sut();
+    sut.resource = resource;
+    sut.description = description;
+    result = sut.getHelpInformation();
+    // Then
+    expect(result).toBe(expectedDescription);
+  });
+
+  it('should be able to generate a help for the CLI and add the options detail', () => {
+    // Given
+    const optionOne = {
+      name: 'type',
+      instruction: '-t, --type [type]',
+      description: 'Set the type',
+    };
+    const optionTwo = {
+      name: 'env',
+      instruction: '-e, --env [env]',
+      description: 'Set the environment',
+    };
+    const optionThree = {
+      name: 'build',
+      instruction: '-b, --build',
+      description: 'Set the build',
+    };
+    const resource = 'my-resource';
+    const description = 'Something';
+    class Sut extends CLIGeneratorSubCommand {}
+    let sut = null;
+    let result = null;
+    const expectedDescription = ` - '${resource}': ${description}` +
+      '\n   Options:\n' +
+      `\n   -t, --type [type]  ${optionOne.description}` +
+      `\n   -e, --env [env]    ${optionTwo.description}` +
+      `\n   -b, --build        ${optionThree.description}`;
+    // When
+    sut = new Sut();
+    sut.resource = resource;
+    sut.description = description;
+    sut.addOption(optionOne.name, optionOne.instruction, optionOne.description);
+    sut.addOption(optionTwo.name, optionTwo.instruction, optionTwo.description);
+    sut.addOption(optionThree.name, optionThree.instruction, optionThree.description);
+    result = sut.getHelpInformation();
+    // Then
+    expect(result).toBe(expectedDescription);
+  });
+
+  it('should throw an error if the `generate` method is not overwritten', () => {
+    // Given
+    class Sut extends CLIGeneratorSubCommand {}
+    let sut = null;
+    // When/Then
+    sut = new Sut();
+    return sut.generate()
+    .then(() => {
+      expect(true).toBeFalse();
+    })
+    .catch((result) => {
+      expect(result).toBeInstanceOf(Error);
+      expect(result.message).toMatch(/This method must to be overwritten/i);
+    });
+  });
+});
